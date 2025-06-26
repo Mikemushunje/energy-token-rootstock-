e7f1c040d2e52971c0a2330e3f7106404c62a70c
Mikemushunje <Michaelmushunje1@gmail.com>
2025-06-26 17:33:10 +0200

    Create EnergyToken.sol

---
+++ 1. contracts/EnergyToken.sol
@@ -0,0 +1,19 @@
+ 1. contracts/EnergyToken.sol
+
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract EnergyToken is ERC20, Ownable {
+    constructor() ERC20("EnergyToken", "ECT") {}
+
+    function mint(address to, uint256 amount) external onlyOwner {
+        _mint(to, amount);
+    }
+
+    function burn(address from, uint256 amount) external onlyOwner {
+        _burn(from, amount);
+    }
+}
